2cf2e421a2ed68ba06d201c73c414e604b6ca462
itsmasad <muhammadasadofficial@gmail.com>
2024-05-16 16:57:53 +0500

    Updated code

--- PageObject/Search.py
+++ PageObject/Search.py
@@ -2,13 +2,12 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class Search:
-    textbox_Search_xpath = "//*[@class='gLFyf'']"
+    textbox_Search_xpath = "//*[@class='gLFyf']"
 
 
-    def __init__(self,setUp):
-        driver = setUp
+    def __init__(self, setUp):
+        self.driver = setUp
 
     def search_withdata(self):
-        driver = webdriver.Chrome()
-        driver.find_element(By.XPATH,Search.textbox_Search_xpath).send_keys("Amazon")
+        self.driver.find_element(By.XPATH,Search.textbox_Search_xpath).send_keys("Amazon")
         
--- testCases/test_Login.py
+++ testCases/test_Login.py
@@ -1,7 +1,13 @@
 from PageObject.Search import Search
+import time
 
 class TestLogin:
-    def test_Login_001(self,setUp):
+    def test_Login_001(self, setUp):
         driver = setUp
         driver.get("https://www.google.com")
-
+        time.sleep(2)
+        # Create an instance of the Search class
+        search_page = Search(driver)
+        # Call the search_withdata method
+        search_page.search_withdata()
+        time.sleep(2)
