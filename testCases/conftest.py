259d6e3aeffe482cdd76a29578700db87baf4874
itsmasad <muhammadasadofficial@gmail.com>
2024-05-16 17:04:19 +0500

    Updated cross browser support

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -5,9 +5,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture(scope="class", autouse=True)
-def setUp():
-    driver = webdriver.Chrome()
+@pytest.fixture(scope="class", autouse=True,params=["chrome","firefox","edge"])
+def setUp(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
+    elif request.param == "edge":
+        driver = webdriver.Edge()
+
+
+    
     driver.maximize_window()
     yield driver
     driver.quit()
