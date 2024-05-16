7ca543a110dab6f8bffec10e21034f57fed0a052
itsmasad <muhammadasadofficial@gmail.com>
2024-05-16 15:55:00 +0500

    updated code

--- testCases/conftest.py
+++ testCases/conftest.py
@@ -1,13 +1,13 @@
-import pytest
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# import pytest
+# from selenium import webdriver
+# from selenium.webdriver.common.by import By
+# from selenium.webdriver.common.keys import Keys
+# from selenium.webdriver.support.ui import WebDriverWait
+# from selenium.webdriver.support import expected_conditions as EC
 
-@pytest.fixture(scope="class",autouse=True)
-def setUp():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
-    yield driver
-    driver.quit()
+# @pytest.fixture(scope="class",autouse=True)
+# def setUp():
+#     driver = webdriver.Chrome()
+#     driver.maximize_window()
+#     yield driver
+#     driver.quit()
--- testCases/test_Login.py
+++ testCases/test_Login.py
@@ -1,10 +1,30 @@
-import softest
 import pytest
+import softest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+@pytest.fixture(scope="class", autouse=True)
+def setUp():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
 
-class test_Login_Cases:
-    @staticmethod
-    @pytest.mark.smock()
-    def test_Login_001():
-        print("Test has been executed")
+class TestLogin(softest.TestCase):
+    def test_Login_001(self,setUp):
+        driver = setUp
+        driver.get("https://www.google.com")
+        # actualTitle = driver.title
+        # self.soft_assert(self.assertEqual, actualTitle, 'Googlelkj ', 'Title does not match')
+        # if actualTitle == "Googlelkj ":
+        #     print("Test is Passed")
+        # else:
+        #     print("Test is failed")
+        # self.assert_all()
+
+    # def test_Login_002(self):
+    #     driver = webdriver.Chrome()
+    #     driver.get("https://www.google.com")
